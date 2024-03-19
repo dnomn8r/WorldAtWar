7ab0ee1c5f795ec427362234501caef7acf5d5f7
Frano Sailer <dnomn8r@gmail.com>
2024-03-19 00:27:11 -0500

    added canals to world map

---
+++ Assets/World Map/Scripts/Canal.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canal : MonoBehaviour{
+
+	[SerializeField] private List<LandZone> owners;
+
+	[SerializeField] private SeaZone firstSeaZone;
+
+	[SerializeField] private SeaZone secondSeaZone;
+
+}
+
