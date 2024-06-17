e1c0293bf3572ef8a98ea3c9dbdbfec8d8d93373
FRONTIER\fsailer <frano@complexgames.com>
2024-06-17 12:59:38 -0500

    much better unit display

--- Assets/Scripts/Editor/ZoneEditor.cs
+++ Assets/Scripts/Editor/ZoneEditor.cs
@@ -1,127 +1,126 @@
-using UnityEditor;
-using UnityEngine;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomEditor(typeof(Zone))]
-public class InteractableItemsEditor : Editor {
+//[CustomEditor(typeof(Zone))]
+//public class InteractableItemsEditor : Editor {
 
-	//[InitializeOnLoad]
-	public class Startup {
+//	public class Startup {
 
-		[UnityEditor.Callbacks.DidReloadScripts]
-		private static void ScriptsHasBeenReloaded() {
-			SceneView.duringSceneGui += DuringSceneGui;
-		}
-		[UnityEditor.Callbacks.DidReloadScripts]
-		static Startup() {
+//		[UnityEditor.Callbacks.DidReloadScripts]
+//		private static void ScriptsHasBeenReloaded() {
+//			SceneView.duringSceneGui += DuringSceneGui;
+//		}
+//		[UnityEditor.Callbacks.DidReloadScripts]
+//		static Startup() {
 
-			Selection.selectionChanged += OnSelectionChanged;
-		}
+//			Selection.selectionChanged += OnSelectionChanged;
+//		}
 
-		private static void DuringSceneGui(SceneView sceneView) {
+//		private static void DuringSceneGui(SceneView sceneView) {
 
-			if (Application.isPlaying) {
-				return;
-			}
+//			if (Application.isPlaying) {
+//				return;
+//			}
 
-			Event e = Event.current;
+//			Event e = Event.current;
 
-			if (e.type == EventType.KeyDown) {
+//			if (e.type == EventType.KeyDown) {
 
-				if (Selection.gameObjects.Length > 0) {
+//				if (Selection.gameObjects.Length > 0) {
 
-					Zone zone = Selection.gameObjects[0].GetComponent<Zone>();
+//					Zone zone = Selection.gameObjects[0].GetComponent<Zone>();
 
-					if (zone != null) {
+//					if (zone != null) {
 
-						if (e.keyCode == KeyCode.Backspace) {
+//						if (e.keyCode == KeyCode.Backspace) {
 
-							zone.Adjacencies.Clear();
+//							zone.Adjacencies.Clear();
 
-							zone.ToggleSelection(true);
+//							zone.ToggleSelection(true);
 
-							//Debug.Log("clearing selection from: " + zone.name);
+//							//Debug.Log("clearing selection from: " + zone.name);
 						
-						}else if(e.keyCode == KeyCode.Return) {
+//						}else if(e.keyCode == KeyCode.Return) {
 
-							for(int i = 1; i < Selection.gameObjects.Length; i++) { 
+//							for(int i = 1; i < Selection.gameObjects.Length; i++) { 
 							
-								Zone adjacentZone = Selection.gameObjects[i].GetComponent<Zone>();
+//								Zone adjacentZone = Selection.gameObjects[i].GetComponent<Zone>();
 
-								if(adjacentZone == null) { continue; }
+//								if(adjacentZone == null) { continue; }
 
-								if (!zone.Adjacencies.Contains(adjacentZone)) {
-									zone.Adjacencies.Add(adjacentZone);
-								}
+//								if (!zone.Adjacencies.Contains(adjacentZone)) {
+//									zone.Adjacencies.Add(adjacentZone);
+//								}
 
-								if(!adjacentZone.Adjacencies.Contains(zone)) {
-									adjacentZone.Adjacencies.Add(zone);	
-								}
-							}
+//								if(!adjacentZone.Adjacencies.Contains(zone)) {
+//									adjacentZone.Adjacencies.Add(zone);	
+//								}
+//							}
 
-							zone.ToggleSelection(true);
+//							zone.ToggleSelection(true);
 
-							//Debug.Log("setting selection to: " + zone.name);
+//							//Debug.Log("setting selection to: " + zone.name);
 
-						} else if (e.keyCode == KeyCode.P && zone is LandZone) {
+//						} else if (e.keyCode == KeyCode.P && zone is LandZone) {
 
-							LandZone landZone = (LandZone)zone;
+//							LandZone landZone = (LandZone)zone;
 
-							for (int i = 1; i < Selection.gameObjects.Length; i++) {
+//							for (int i = 1; i < Selection.gameObjects.Length; i++) {
 
-								LandZone adjacentZone = Selection.gameObjects[i].GetComponent<LandZone>();
+//								LandZone adjacentZone = Selection.gameObjects[i].GetComponent<LandZone>();
 
-								if (adjacentZone == null) { continue; }
+//								if (adjacentZone == null) { continue; }
 
-								if (!landZone.HazardousAdjacencies.Contains(adjacentZone)) {
-									landZone.HazardousAdjacencies.Add(adjacentZone);
-								}
+//								if (!landZone.HazardousAdjacencies.Contains(adjacentZone)) {
+//									landZone.HazardousAdjacencies.Add(adjacentZone);
+//								}
 
-								if (!adjacentZone.HazardousAdjacencies.Contains(landZone)) {
-									adjacentZone.HazardousAdjacencies.Add(landZone);
-								}
-							}
+//								if (!adjacentZone.HazardousAdjacencies.Contains(landZone)) {
+//									adjacentZone.HazardousAdjacencies.Add(landZone);
+//								}
+//							}
 
-							landZone.ToggleSelection(true);
+//							landZone.ToggleSelection(true);
 
-							//Debug.Log("setting selection to: " + zone.name);
-						}
-					}
-				}
-			}
-		}
+//							//Debug.Log("setting selection to: " + zone.name);
+//						}
+//					}
+//				}
+//			}
+//		}
 
-		private static Zone currentZone;
-		private static void OnSelectionChanged() {
+//		private static Zone currentZone;
+//		private static void OnSelectionChanged() {
 
-			if (Application.isPlaying) {
-				return;
-			}
+//			if (Application.isPlaying) {
+//				return;
+//			}
 
-			for (int i = 0; i < Selection.gameObjects.Length; i++) {
+//			for (int i = 0; i < Selection.gameObjects.Length; i++) {
 
-				Zone selectedZone = Selection.gameObjects[0].GetComponent<Zone>();
+//				Zone selectedZone = Selection.gameObjects[0].GetComponent<Zone>();
 				
-				if (selectedZone != currentZone && currentZone != null) {
+//				if (selectedZone != currentZone && currentZone != null) {
 
-					//Debug.Log("zone: " + currentZone.name + " deselected");
-					currentZone.ToggleSelection(false);
-				}
+//					//Debug.Log("zone: " + currentZone.name + " deselected");
+//					currentZone.ToggleSelection(false);
+//				}
 
-				currentZone = selectedZone;
+//				currentZone = selectedZone;
 
-				if (selectedZone != null) {
+//				if (selectedZone != null) {
 
-					//Debug.Log("we've selected: " + selectedZone.name);
+//					//Debug.Log("we've selected: " + selectedZone.name);
 
-					currentZone.ToggleSelection(true);
-				}
-			}
+//					currentZone.ToggleSelection(true);
+//				}
+//			}
 
-			if(Selection.gameObjects.Length == 0 && currentZone != null) {
-				currentZone.ToggleSelection(false);
-				currentZone = null;
-			}
-		}
-	}
+//			if(Selection.gameObjects.Length == 0 && currentZone != null) {
+//				currentZone.ToggleSelection(false);
+//				currentZone = null;
+//			}
+//		}
+//	}
 
-}
+//}
