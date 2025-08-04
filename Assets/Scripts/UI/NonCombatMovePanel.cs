7d3bc5c79a59788d67296fc46b2b1f5aefa3a023
FRONTIER\fsailer <frano@complexgames.com>
2025-08-04 09:38:47 -0500

    start of non combat move panel

---
+++ Assets/Scripts/UI/NonCombatMovePanel.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static Zone;
+
+public class NonCombatMovePanel : MonoBehaviour {
+
+	[SerializeField] private TextMeshProUGUI nameField;
+
+	[SerializeField] private UnitDetailEntry unitEntry;
+
+	[SerializeField] private Transform unitEntryStartMount;
+
+	private Zone currentZone;
+
+	private List<UnitDetailEntry> unitEntries = new List<UnitDetailEntry>();
+
+
+	public void SetZone(Zone selectedZone) {
+
+		// clear previous entries
+		for (int i = 0;i< unitEntries.Count; i++) {
+			Destroy(unitEntries[i].gameObject);
+		}
+
+		unitEntries.Clear();
+
+		currentZone = selectedZone;
+
+		nameField.text = currentZone.name;
+
+		//List<UnitOwnershipEntry> unitOwnerships = selectedZone.GetUnits();
+
+		List<UnitInstance> allUnits = selectedZone.GetUnits();
+
+		//Debug.Log("selected zone: " + currentZone.name);
+
+		float currentOffset = 0.0f;
+		float entrySize = 40.0f;
+
+		Dictionary<string, UnitOwnershipEntry> unitOwnershipDictionary = new Dictionary<string, UnitOwnershipEntry>();	
+
+		foreach (UnitInstance unitInstance in allUnits) {
+
+			string key = unitInstance.owner.name + unitInstance.unit.name;
+
+			if (!unitOwnershipDictionary.ContainsKey(key)) {
+
+				unitOwnershipDictionary.Add(key, new UnitOwnershipEntry(unitInstance, 1));
+			} else {
+
+				UnitOwnershipEntry entry = unitOwnershipDictionary[key];
+				entry.count += 1;
+				unitOwnershipDictionary[key] = entry;
+			}
+		}
+
+		List<UnitOwnershipEntry> unitOwnerships = new List<UnitOwnershipEntry>(unitOwnershipDictionary.Values);
+
+		foreach (UnitOwnershipEntry currentOwnershipEntry in unitOwnerships) {
+
+            UnitDetailEntry newEntry = Instantiate<UnitDetailEntry>(unitEntry, unitEntryStartMount);
+			//newEntry.transform.localPosition = new Vector3(0, -currentOffset, 0);
+
+			newEntry.SetUnit(currentOwnershipEntry);
+
+			unitEntries.Add(newEntry);
+
+			currentOffset += entrySize;
+
+			//Debug.Log("unit: " + currentOwnershipEntry.unit.name + " x" + currentOwnershipEntry.count + " owned by: " + currentOwnershipEntry.owner.name);
+		}
+
+	}
+
+}
